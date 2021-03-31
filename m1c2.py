35045253a24e3f1574caf238b3aab21c2de870c8
shubhamkj5 <shubhamkj2405@gmail.com>
2021-03-31 19:25:15 +0100

    Addes sales_subset.csv

--- m1c2.py
+++ m1c2.py
@@ -1,2 +1,10 @@
-sdfslih
-sdfgd
+import pandas as pd
+
+data=pd.read_csv("sales_subset.csv")
+
+print(data.info())
+# Select the individuals column
+individuals = homelessness["individuals"]
+
+# Print the head of the result
+print(individuals.head())
